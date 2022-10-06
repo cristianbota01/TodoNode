bcf62f3413565a1475474e266d7b67f01ad8e717
cristianbota001 <c.bota@ifortech.com>
2022-10-06 12:37:35 +0200

    goood

--- server.js
+++ server.js
@@ -1,46 +1,67 @@
-const express = require("express")
+const express = require("express");
+const { send } = require("express/lib/response");
 const mongoose = require("mongoose")
 
 const app = express()
-/* app.use(express.json()); */
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost/todo_list", { useNewUrlParser: true })
-const db = mongoose.connection
 
 const todo_schema = new mongoose.Schema({
-    todo_name: String,
-    todo_img_path: String,
-    todo_description: String,
-    created_at: {
-        type: Date,
-        default: Date.now()
-    },
-    complete: Boolean
+    username:String,
+    password:String,
+    todos:[
+        {
+            todo_name: String,
+            todo_img_path: String,
+            todo_description: String,
+            created_at: {
+                type: Date,
+                default: Date.now()
+            },
+            complete: Boolean
+        }
+    ]
+    
 })
 
 const todos = mongoose.model("todos", todo_schema)
 
-app.get("/", async (req, res) => {
-    console.log(await todos.find())
-    res.send("null")
-}).get("/ciao", (req, res) => {
-    res.send("ciao")
-})
-
 app.post("/", async (req, res) => {
 
-    /* new_todo = new todos({
-        todo_name: "icoa",
-        todo_img_path: "e2rrw",
-        todo_description: "rwjhfvjhfgjhgfgejf",
-        complete: false
+    new_todo = new todos({
+        username:req.body.username,
+        password:req.body.password,
+        todos:[
+            
+        ]
     })
 
-    new_todo.save() */
+    new_todo.save()
+    res.json(req.body)
+})
 
-    console.log(req.body)
+app.post("/add_todo", (req, res) => {
+    
+    todos.findByIdAndUpdate(req.body.user_id, 
+        {
+            "$push" : {"todos" : {
+                "todo_name": req.body.todo_name,
+                "todo_img_path": req.body.todo_img_path,
+                "todo_description": req.body.todo_description,
+                "complete": false
+            }}
+        },
 
+        function (err, managerparent) {
+            if (err) throw err;
+            console.log(managerparent);
+        }
+    )
+
+    res.send("ok")
 })
 
 app.listen(3000, () => {
